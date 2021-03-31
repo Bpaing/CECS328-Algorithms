1897528a31841484063e9610f7e1a44a257b9d1d
Brendan Paing <54960564+Bpaing@users.noreply.github.com>
2021-03-30 17:33:12 -0700

    Update main
    Formatting for main method

--- Lab 5/src/Lab5.java
+++ Lab 5/src/Lab5.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class Lab5 {
 
-    
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.print("Enter size n: ");
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = random.nextInt(41) - 20;
+
+        
+    }
 }
