66a14e07c71f55fa267ac93b1ac1dd5d329629fb
Brendan Paing <54960564+Bpaing@users.noreply.github.com>
2021-04-25 21:31:21 -0700

    Lab 7 Created
    This lab deals with graphs.

---
+++ Lab 7 - Graphs/src/Lab7.java
@@ -0,0 +1,17 @@
+public class Lab7 {
+
+    public static void main(String args[])
+    {
+
+    }
+
+    public static void BFS()
+    {
+
+    }
+
+    public static boolean isBipartite()
+    {
+
+    }
+}
