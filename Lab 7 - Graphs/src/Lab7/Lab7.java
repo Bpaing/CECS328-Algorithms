d47a06457c7fb4d53b6e05684e69d6d26ef3701c
Brendan Paing <54960564+Bpaing@users.noreply.github.com>
2021-04-26 20:06:25 -0700

    Possible future implementation

--- Lab 7 - Graphs/src/Lab7/Lab7.java
+++ Lab 7 - Graphs/src/Lab7/Lab7.java
@@ -1,5 +1,8 @@
 package Lab7;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Lab7 {
@@ -17,5 +20,43 @@ public class Lab7 {
     {
         return true;
     }
+    
 
 }
+
+/*
+public class Node {
+
+    private char key;
+    private int dst;
+    private Node parent;
+    private ArrayList<Node> adj;
+
+    public Node(char name) {
+        key = name;
+        adj = new ArrayList<>();
+        dst = 0;
+        parent = null;
+    }
+
+    public void add(Node node) {
+        adj.add(node);
+        node.adj.add(this);
+    }
+
+    public static void BFS(Node initial) {
+        Queue<Node> q = new LinkedList();
+        q.add(initial);
+        while (q.size() > 0) {
+            Node nextNode = q.remove();
+            for (Node n : nextNode.adj) {
+                if (n.parent == null) {
+                    q.add(nextNode);
+                    n.parent = nextNode;
+                    n.dst = nextNode.dst + 1;
+                }
+            }
+        }
+    }
+}
+ */
